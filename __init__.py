d1a175a00e3d2c088f548786f75c1cb5238ef83e
Mrpatekful <purgai.patrik@gmail.com>
2018-08-17 10:02:53 +0200

    File templates added

--- __init__.py
+++ __init__.py
@@ -1,3 +1,11 @@
+"""
+
+@author:    Patrik Purgai
+@copyright: Copyright 2018, ClusterFlow
+@license:   MTA
+@email:     purgai.patrik@gmail.com
+@date:      2018.08.17.
+"""
 
 from clustering import MeanShift
 from clustering import KMeans
--- clustering.py
+++ clustering.py
@@ -1,5 +1,10 @@
 """
 
+@author:    Patrik Purgai
+@copyright: Copyright 2018, ClusterFlow
+@license:   MTA
+@email:     purgai.patrik@gmail.com
+@date:      2018.08.17.
 """
 
 from __future__ import absolute_import
